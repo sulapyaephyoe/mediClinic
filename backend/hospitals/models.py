f5ca608545879425ec1c22c250b30dabb158355d
thiri-nandar-nyan <thirinandarnyan220994@gmail.com>
2022-07-28 15:56:45 +0630

    add hospital

--- backend/hospitals/models.py
+++ backend/hospitals/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class hospitals(models.Model):
+
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    location = models.CharField(max_length=355)
+    website = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+
+    def _str_(self):
+        return self.name
+
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory} from 'vue-router'
 // import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Posts from './views/PostsData'
 import Tests from './views/TestData'
+import AddHospital from './views/hospitals/AddHospital'
 
 const routes = [
     {
@@ -14,6 +15,11 @@ const routes = [
         name: 'tests',
         component: Tests, 
     },
+    {
+        path: '/hospitals/add-hospital',
+        name: 'AddHospital',
+        component: AddHospital,
+      },
 ]
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
