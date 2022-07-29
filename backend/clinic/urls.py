fd8a107ff28e65278ac273bab86eeb6065866d85
Su La Pyae Phyoe <slpp.sulapyaephyoe@gmail.com>
2022-07-29 14:30:46 +0630

    Update urls.py

--- backend/clinic/urls.py
+++ backend/clinic/urls.py
@@ -3,12 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls'))
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
-    
-  
     path('', include('app.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 
     path('doctors/', include('doctors.urls'))
     path('', include('hospitals.urls')),
