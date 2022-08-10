136f7cd2e39f03595d7c8cbd2625fec8ada06c8b
slpp <slpp.sulapyaephyoe@gmail.com>
2022-08-10 20:19:41 +0630

    before merge

--- backend/hospitals/serializers.py
+++ backend/hospitals/serializers.py
@@ -5,7 +5,11 @@ class HospitalSerializer(serializers.ModelSerializer):
     class Meta:
         model = hospitals
 <<<<<<< HEAD
+<<<<<<< HEAD
         fields =['id','name', 'phone','address','website','type','latitude','longitude','photo','video']
+=======
+        fields =['name', 'phone','address','website','type','latitude','longitude','photo','video']
+>>>>>>> main
 =======
         fields =['name', 'phone','address','website','type','latitude','longitude','photo','video']
 >>>>>>> main
