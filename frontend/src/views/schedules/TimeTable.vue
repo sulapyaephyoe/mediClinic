a359018d5d449e26e9c78f7bbe4950bbe4d18173
slpp <slpp.sulapyaephyoe@gmail.com>
2022-08-19 08:25:21 +0630

    usersControlupdate

--- frontend/src/views/schedules/TimeTable.vue
+++ frontend/src/views/schedules/TimeTable.vue
@@ -1,67 +1,124 @@
+
 <template>
-  <div>
-   <button
-  v-tooltip="{
-  content: msg,
-  placement: placement,
-}"
->
-  Hover me
-</button>
-  </div>
+<div class="class">
+  <FullCalendar :options="calendarOptions" />
+  <span class="displayText" id="displayText">Hi I am ToolTip</span>
+</div>
 </template>
 <script>
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from '@fullcalendar/interaction'
+
+import { getAPI } from '../../axios-api'
+
 export default {
-  props: { 
-    text: {
-      type: String,
-      required: true
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, timeGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        eventClick: this.showEventClick,
+        events: [{'title': 'hello','daysOfWeek': [0]}],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay"
+        },
+      }
     }
   },
+  beforeCreate() {
+  },
+  created() {
+    this.detail_specialist('Dermatology')
+  },
+  methods: {
+
+    detail_specialist(sname){
+      getAPI.get(`schedule/get_schedule_data/${sname}`)
+      .then(response => {
+        this.join_data = response.data.join_data
+        this.doctor_data = response.data.doctor_data
+        this.hospital_data = response.data.hospital_data
+        this.add_event()
+      })
+      .catch(error => {
+          console.log(error)
+      })
+    },
+
+    add_event() {
+      this.join_data.forEach(element => {
+        let doctor = this.doctor_data.find(x => x.id === element.doctor_id)
+        let hospital = this.hospital_data.find(x => x.id === element.hospital_id)
+        let content = "Dr."+ doctor.firstName + " " + doctor.lastName + " \r\nAt " + hospital.name + "hospital."
+        let k = this.dayKey(element.day)   
+        this.calendarOptions.events = [ 
+          ...this.calendarOptions.events, {
+            title: content, 
+            daysOfWeek: [k]
+          }
+        ];
+      });     
+    },
+
+    dayKey(day) {
+      var dkey=0;
+      switch(day) {
+        case 'Sunday': dkey=0;break;
+        case 'Monday': dkey=1;break;
+        case 'Tuesday': dkey=2;break;
+        case 'Wednesday': dkey=3;break;
+        case 'Thursday': dkey=4;break;
+        case 'Friday': dkey=5;break;
+        case 'Saturday': dkey=6;break;
+      }
+      return dkey;
+    },
+
+    showEventClick(arg) {
+      console.log(arg.event.title)
+      var popup = document.getElementById("displayText");
+      popup.classList.toggle("show");
+    },
+  }
 }
 </script>
-
 <style scoped>
-.tooltip-container { 
-  position: relative;
-  display: inline-block;
-  margin:100px;
+.class {
+-webkit-user-select: none;
+position: relative;
 }
-
-.tooltip-container:hover .tooltip{
-  opacity: 1;
-
+.displayText {
+position: absolute;
+bottom: -230%;
+left: 50%;
+margin-left: -80px;
+width: 160px;
+background-color: aqua;
+color: #fff;
+color: red;
+text-align: center;
+border-radius: 6px;
+padding: 8px 0;
+visibility: hidden;
 }
-
-.tooltip { 
-  color: #ffffff;
-  text-align: center;
-  padding: 5px 0;
-  border-radius: 2px;
-
-  width: 120px;
-  bottom: 100%;
-  left: 50%;
-  margin-left: -60px;
-
-  opacity: 0;
-  transition: opacity 1s;
-
-  position: absolute;
-  z-index: 1;
-
-  background: #000000;
+.displayText::before {
+content: "";
+border-width: 5px;
+border-style: solid;
+top: -28%;
+left: 45%;
+border-color: transparent transparent yellow transparent;
+position: absolute;
 }
-
-.text::after {
-  content: " ";
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  margin-left: -5px;
-  border-width: 5px;
-  border-style: solid;
-  border-color: #000000 transparent transparent transparent;
-
+.show {
+visibility: visible;
 }
 </style>
